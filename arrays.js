05999eb72b22290b24754e3475275578d823b660
Daniel Singer <dannys430@yahoo.com>
2019-07-02 19:35:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  var new_array = (element, array);
-  return new_array;
+  return[element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
