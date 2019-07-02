8c6cec1b3f33741ed45bc1149d2a78312869868a
Daniel Singer <dannys430@yahoo.com>
2019-07-02 19:28:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,8 +11,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  var new_array = (element, array);
-  var array = (element, ...new_array);
+  var new_array = (element, array)
+  var array = (element, ...new_array)
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
